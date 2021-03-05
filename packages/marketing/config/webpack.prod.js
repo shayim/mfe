9d785dccb7fb3578052baacf1b6f0c261d35e1ad
shayim <shaynv@hotmail.com>
2021-03-05 17:08:03 +0800

    add marketing.yml

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -6,7 +6,8 @@ const common = require('./webpack.common')
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest'
   },
   plugins: [
     new ModuleFederationPlugin({
