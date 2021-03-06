1db6ce9bab437af3e3576c57c9c2309b120d2a07
shayim <shaynv@hotmail.com>
2021-03-06 10:21:53 +0800

    update createGenerateClassName

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles'
 import Header from './components/Header'
 import Marketing from './components/marketing'
 
+const className = createGenerateClassName({
+  productionPrefix: 'co'
+})
+
 export default () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <Marketing />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={className}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Marketing />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
--- packages/marketing/src/app.js
+++ packages/marketing/src/app.js
@@ -1,22 +1,27 @@
 import React from 'react'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { StylesProvider } from '@material-ui/core/styles'
+import {
+  StylesProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const className = createGenerateClassName({
+  productionPrefix: 'ma'
+})
+
 const App = () => {
   return (
-    <div className="marketing">
-      <StylesProvider>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path="/pricing" component={Pricing} />
-            <Route path="/" component={Landing} />
-          </Switch>
-        </BrowserRouter>
-      </StylesProvider>
-    </div>
+    <StylesProvider generateClassName={className}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/pricing" component={Pricing} />
+          <Route path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
+    </StylesProvider>
   )
 }
 
